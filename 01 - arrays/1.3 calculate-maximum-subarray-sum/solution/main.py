9747af182bb960e2ddf18ec535c0f67fc216e848
Imad Jerra <smartshader@gmail.com>
2023-11-24 16:20:22 -0500

    problem 1.3

---
+++ 01 - arrays/1.3 calculate-maximum-subarray-sum/solution/main.py
@@ -0,0 +1,15 @@
+"""
+Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+"""
+
+given = [34, -50, 42, 14, -5, 86]
+
+expected = 137  # [42, 14, -5, 86]
+
+
+def solution(arr):
+    print('Not implemented')
+
+
+if __name__ == '__main__':
+    assert solution(given) == expected
