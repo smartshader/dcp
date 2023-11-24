b736fc28a0b5b0876f3229ecbee576b1925687bb
Imad Jerra <smartshader@gmail.com>
2023-11-24 14:38:47 -0500

    problem 1.1

---
+++ 01 - arrays/1.1 get-product-of-all-other-elements/main.py
@@ -0,0 +1,11 @@
+given = [1, 2, 3, 4, 5]
+
+expected = [120, 60, 40, 30, 24]
+
+
+def solution(arr):
+    print('Not implemented')
+
+
+if __name__ == '__main__':
+    assert solution(given) == expected
