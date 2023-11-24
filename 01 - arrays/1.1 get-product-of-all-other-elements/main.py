ee6a4baad560045f9b797417a320f3e6b231b57b
Imad Jerra <smartshader@gmail.com>
2023-11-24 14:41:59 -0500

    problem 1.1 explanation

--- 01 - arrays/1.1 get-product-of-all-other-elements/main.py
+++ 01 - arrays/1.1 get-product-of-all-other-elements/main.py
@@ -1,3 +1,8 @@
+"""
+Given an array of integers, return a new array such that each element at index 'i' of the new array
+is the product of all numbers in the original array except the one at 'i'.
+"""
+
 given = [1, 2, 3, 4, 5]
 
 expected = [120, 60, 40, 30, 24]
