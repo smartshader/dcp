5444dd0dc4de59c8e444938ead92a8e3cefc9b43
Imad Jerra <smartshader@gmail.com>
2023-11-24 15:23:35 -0500

    problem 1.2

---
+++ 01 - arrays/1.2 locate-smallest-window-to-be-sorted/problem/main.py
@@ -0,0 +1,16 @@
+"""
+Given an array of integers that are out of order, determine the bounds of the smallest window that must be sorted
+in order for the entire array to be sorted.
+"""
+
+given = [3, 7, 5, 6, 9]
+
+expected = (1, 3)
+
+
+def solution(arr):
+    print('Not implemented')
+
+
+if __name__ == '__main__':
+    assert solution(given) == expected
